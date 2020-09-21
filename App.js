9af6cdccd5aaacb8b2c34ee78f649a9e0ecb46dd
Joel Yek <joevo2@gmail.com>
2020-09-21 23:45:53 +0800

    Added react native masonry

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import MasonryList from "react-native-masonry-list";
+
+const IMAGELISTS = [
+  { uri: "https://source.unsplash.com/user/erondu" },
+  { uri: "https://source.unsplash.com/collection/190727" },
+  { uri: "https://source.unsplash.com/daily" },
+  { uri: "https://source.unsplash.com/1600x900/?nature,water" },
+  { uri: "https://source.unsplash.com/WLUHO9A_xik" },
+  { uri: "https://source.unsplash.com/random" },
+  { uri: "https://source.unsplash.com/collection/190227" },
+  { uri: "https://source.unsplash.com/collection/191727" },
+]
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <MasonryList
+      images={IMAGELISTS}
+      imageContainerStyle={{
+        borderRadius: 20,
+      }}
+      onPressImage={(item, index) => {
+        alert(JSON.stringify(item))
+      }}
+    />
   );
 }
 
